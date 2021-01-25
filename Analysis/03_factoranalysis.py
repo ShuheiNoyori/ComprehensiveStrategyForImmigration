1016f6254c63bfc74d19586bb936403ee45501e4
Shuhei Noyori <noyori.shuhei@gmail.com>
2021-01-25 09:33:37 +0900

    変数の指定方法変更・CSV書き出すように変更

--- Analysis/03_factoranalysis.py
+++ Analysis/03_factoranalysis.py
@@ -4,13 +4,13 @@ import os
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from factor_analyzer import FactorAnalyzer as FA
-from matplotlib import pyplot as plt
-plt.rcParams['font.family'] = 'Hiragino Sans'
 
 ##############################################
 # Parameters
 ##############################################
-n_factors = 10
+n_factors = 12
+thresh = 0.3
+maxwords = 1000
 
 ##############################################
 # Functions
@@ -39,19 +39,24 @@ def FactorAnalysisFromDataFrame(df, n_factors):
 ##############################################
 # データ読込
 path_to_file = 'path_to_file'
-filename = 'filename.csv' # 01_vectorize.py で出力したファイルの使用を想定
+filename = 'tfidf_{}.csv'.format(maxwords) # 01_vectorize.py で出力したファイルの使用を想定
 
 df = pd.read_csv(os.path.join(path_to_file, filename), index_col=0)
 
 # 指定した因子数で因子分析
 df_factor_score, df_factor_loading = FactorAnalysisFromDataFrame(df, n_factors)
 
-# 因子負荷量が0.4未満の変数を消す
+# 因子負荷量がthresh未満の変数を消す
 df_selected_factor_score, df_selected_factor_loading = df_factor_score, df_factor_loading
 df_selected = df
-selectedwords = [i[0] for i in  df_selected_factor_loading[df_selected_factor_loading>=0.4].dropna(how='all').index]
+selectedwords = [i[0] for i in  df_selected_factor_loading[df_selected_factor_loading>=thresh].dropna(how='all').index]
 
 while len(selectedwords) < len(df_selected_factor_loading):
     df_selected = df[selectedwords]
     df_selected_factor_score, df_selected_factor_loading = FactorAnalysisFromDataFrame(df_selected, n_factors)
-    selectedwords = [i[0] for i in  df_selected_factor_loading[df_selected_factor_loading>=0.4].dropna(how='all').index]
+    selectedwords = [i[0] for i in  df_selected_factor_loading[df_selected_factor_loading>=thresh].dropna(how='all').index]
+
+df_selected_factor_score.to_csv(os.path.join(path_to_file, 'FA_factor_score_from_tfidf_{}.csv'.format(maxwords)), 
+                                             encoding='utf-8-sig') # 因子得点をCSV保存
+df_selected_factor_loading.to_csv(os.path.join(path_to_file, 'FA_factor_loading_from_tfidf_{}.csv'.format(maxwords)), 
+                                               encoding='utf-8-sig') # 因子負荷量をCSV保存
