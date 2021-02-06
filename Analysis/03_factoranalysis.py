dc5f216c57822372fe67f576261664d78cb8ab45
Shuhei Noyori <noyori.shuhei@gmail.com>
2021-02-06 21:16:07 +0900

    [修正] 因子負荷量-0.4以下も入れるようにした

--- Analysis/03_factoranalysis.py
+++ Analysis/03_factoranalysis.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import os
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import StandardScaler
 from factor_analyzer import FactorAnalyzer as FA
 
@@ -10,7 +11,6 @@ from factor_analyzer import FactorAnalyzer as FA
 ##############################################
 n_factors = 12
 thresh = 0.3
-maxwords = 1000
 
 ##############################################
 # Functions
@@ -22,7 +22,7 @@ def FactorAnalysisFromDataFrame(df, n_factors):
     z = sc.transform(df)
     
     # 因子分析
-    fa = FA(n_factors, rotation='oblimin')
+    fa = FA(n_factors, rotation='promax', method='minres')
     fa.fit(z)
     
     df_factor_score = pd.DataFrame(fa.transform(z), 
@@ -39,24 +39,26 @@ def FactorAnalysisFromDataFrame(df, n_factors):
 ##############################################
 # データ読込
 path_to_file = 'path_to_file'
-filename = 'tfidf_{}.csv'.format(maxwords) # 01_vectorize.py で出力したファイルの使用を想定
+filename = 'filename.csv' # 01_vectorize.py で出力したファイルの使用を想定
 
 df = pd.read_csv(os.path.join(path_to_file, filename), index_col=0)
 
 # 指定した因子数で因子分析
 df_factor_score, df_factor_loading = FactorAnalysisFromDataFrame(df, n_factors)
 
-# 因子負荷量がthresh未満の変数を消す
+# 因子負荷量の絶対値がthresh未満の変数を消す
 df_selected_factor_score, df_selected_factor_loading = df_factor_score, df_factor_loading
 df_selected = df
-selectedwords = [i[0] for i in  df_selected_factor_loading[df_selected_factor_loading>=thresh].dropna(how='all').index]
+selectedwords = [i[0] for i in  df_selected_factor_loading[np.logical_or(df_selected_factor_loading >= thresh,
+                                                                         df_selected_factor_loading <= -1*thresh)].dropna(how='all').index]
 
 while len(selectedwords) < len(df_selected_factor_loading):
     df_selected = df[selectedwords]
     df_selected_factor_score, df_selected_factor_loading = FactorAnalysisFromDataFrame(df_selected, n_factors)
-    selectedwords = [i[0] for i in  df_selected_factor_loading[df_selected_factor_loading>=thresh].dropna(how='all').index]
+    selectedwords = [i[0] for i in  df_selected_factor_loading[np.logical_or(df_selected_factor_loading >= thresh,
+                                                                             df_selected_factor_loading <= -1*thresh)].dropna(how='all').index]
 
-df_selected_factor_score.to_csv(os.path.join(path_to_file, 'FA_factor_score_from_tfidf_{}.csv'.format(maxwords)), 
+df_selected_factor_score.to_csv(os.path.join(path_to_file, 'FA_factor_score_from_tfidf.csv'), 
                                              encoding='utf-8-sig') # 因子得点をCSV保存
-df_selected_factor_loading.to_csv(os.path.join(path_to_file, 'FA_factor_loading_from_tfidf_{}.csv'.format(maxwords)), 
+df_selected_factor_loading.to_csv(os.path.join(path_to_file, 'FA_factor_loading_from_tfidf.csv'), 
                                                encoding='utf-8-sig') # 因子負荷量をCSV保存
